a906c85ed355150d4eb55123236e9a1016479add
Debdeep Pal <deep64222@gmail.com>
2022-05-12 17:17:05 +0530

    init

---
+++ backend/server.js
@@ -0,0 +1,65 @@
+let PORT = process.env.PORT || 3000,
+    express = require('express'),
+    fs = require("fs"),
+    app = require('express')(),
+    server = require('http').Server(app);
+    // siofu = require("socketio-file-upload");
+
+// let passport = require("passport");
+// const GoogleStrategy = require("passport-google-oauth20").Strategy;
+// const keys = require("./config");
+
+
+let io = require("socket.io")(server);
+server.listen(PORT);
+console.log(`Server online at http://localhost:${PORT}`);
+
+app.use(express.static('build'))
+// app.use(express.static('./public/uploads'))
+
+
+// app.use(siofu.router)
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+let numuser = 0,
+    chat=[];
+
+io.on("connection", socket => {
+    // console.log("user connected");
+
+    numuser++;
+    console.log("Number of user: " + numuser);
+    socket.emit("receive-chat-history",chat)
+
+    // socket.emit("receive-chat",chat);
+
+    socket.on("send-chat",data=>{
+        socket.broadcast.emit('receive-chat',data)
+        chat.push(data)
+        // console.log(chat)
+    })
+    socket.on("send-file",data=>{
+        socket.broadcast.emit('receive-chat',data)
+        chat.push(data)
+        // console.log(chat)
+    })
+    socket.on("send-video-stream",data=>{
+        socket.broadcast.emit('receive-video-stream',data)
+        // chat.push(data)
+        // console.log(chat)
+    })
+
+
+    // var uploader = new siofu();
+    // uploader.dir = "./public/uploads/";
+    // uploader.listen(socket);
+    //
+    // socket.on("disconnect", () => {
+    //     // console.log("user disconnected");
+    //     numuser--;
+    //     console.log("Number of user: " + numuser);
+    // });
+});
--- src/App.js
+++ src/App.js
@@ -1,25 +1,160 @@
-import logo from './logo.svg';
+import React, {useRef, useState, Component} from 'react';
 import './App.css';
+import io from 'socket.io-client'
+
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth'
+import 'firebase/firestore';
+import {getAuth, signInWithEmailAndPassword, signOut} from 'firebase/auth';
+import 'firebase/analytics';
+
+import {useAuthState, useSignInWithGoogle} from 'react-firebase-hooks/auth';
+import {useCollectionData} from 'react-firebase-hooks/firestore';
+import {initializeApp} from "firebase/app";
+import {getAnalytics} from "firebase/analytics";
+
+const socket = io(`ws://localhost:3000`, {
+    reconnectionDelayMax: 10000,
+    // auth: {
+    //     token: "123"
+    // },
+    // query: {
+    //     "my-key": "my-value"
+    // }
+});
+socket.emit("ping")
+const firebaseConfig = {
+    apiKey: "AIzaSyAqY2IJXJqf9cgaApnF2rs5ur7sDsP0LU4",
+    authDomain: "intellichat-32022.firebaseapp.com",
+    projectId: "intellichat-32022",
+    storageBucket: "intellichat-32022.appspot.com",
+    messagingSenderId: "729039606167",
+    appId: "1:729039606167:web:0b6e9a08d4cb3906d734d2",
+    measurementId: "G-8QSV8ZWF5E"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// console.log(app)
+// const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const firestore = firebase.firestore;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [user, loading, error] = useAuthState(auth);
+
+    return (
+        <div className="App">
+            <header>
+                <h1>🧠intelliChat</h1>
+                <SignOut/>
+            </header>
+
+            <section>
+                {user ? <ChatRoom/> : <SignIn/>}
+            </section>
+
+        </div>
+    );
+}
+
+function SignIn() {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    return (
+        <>
+            <button className="sign-in" onClick={() => signInWithGoogle()}>Sign in with Google</button>
+        </>
+    )
+
+}
+
+function SignOut() {
+    return auth.currentUser && (
+        <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
+    )
+}
+
+class ChatRoom extends Component {
+
+    constructor(props) {
+
+        super(props);
+        this.dummyRef = React.createRef();
+        this.textRef = React.createRef();
+        this.sendMessage = this.sendMessage.bind(this);
+        this.typeMessage = this.typeMessage.bind(this);
+        this.state = {
+            messages: [],
+            formValue: ""
+        }
+    }
+    sendMessage(e) {
+        e.preventDefault();
+        console.log(this.state)
+
+        const {uid, photoURL} = auth.currentUser;
+        this.state.messages.push({
+            text: this.state.formValue,
+            // createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+            uid,
+            photoURL
+        })
+        this.textRef.current.value = ""
+        this.state.formValue = ""
+        this.setState(prevState=>{
+            return this.state;
+        })
+        this.dummyRef.current.scrollIntoView({behavior: 'smooth'});
+    }
+    typeMessage(e){
+        e.preventDefault();
+        this.state.formValue= e.target.value;
+        this.setState(prevState=>{
+            return this.state;
+        })
+        console.log(this.state)
+    }
+    render() {
+
+
+        // const dummy = useRef();
+
+
+
+
+
+
+        return (<>
+            <main>
+                {this.state.messages && this.state.messages.map((msg, i) => <ChatMessage key={i} message={msg}/>)}
+
+                <span ref={this.dummyRef}/>
+
+            </main>
+
+            <form onSubmit={this.sendMessage}>
+
+                <input ref={this.textRef} onChange={this.typeMessage}
+                       placeholder="say something nice"/>
+
+                <button type="submit" disabled={!this.state.formValue}>💬</button>
+
+            </form>
+        </>)
+    }
+}
+
+function ChatMessage(props) {
+    const {text, uid, photoURL} = props.message;
+
+    const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+
+    return (<>
+        <div className={`message ${messageClass}`}>
+            <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'}/>
+            <p>{text}</p>
+        </div>
+    </>)
 }
 
 export default App;
